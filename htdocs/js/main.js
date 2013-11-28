842c7e27b207ee133f8962604532d27a60eab901
Stefan Toman <stefan.toman@gmail.com>
2013-11-28 23:18:43 +0100

    add background image and update logic

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,6 +1,44 @@
-$(function() {
-    var stage = new PIXI.Stage(0x66FF99);
+$(window).load(function() {
+    // setup renderer and stage
     var canvas = $("#game-canvas");
-    var renderer = new PIXI.autoDetectRenderer(canvas.width(), canvas.height(), canvas[0]);
-    renderer.render(stage);
+    var renderer = new PIXI.autoDetectRenderer(
+	    canvas.width(),
+	    canvas.height(),
+	    canvas[0]
+	    );
+    var stage = new PIXI.Stage(0x66FF99);
+    
+    // far background
+    var backgroundFarTexture = PIXI.Texture.fromImage("img/github_game_off_2013_resized.png");
+    var backgroundFar = new PIXI.TilingSprite(
+	    backgroundFarTexture,
+	    2048,
+	    512
+    );
+    backgroundFar.position.x = 0;
+    backgroundFar.position.y = 0;
+    backgroundFar.tilePosition.x = 0;
+    backgroundFar.tilePosition.y = 0;
+    stage.addChild(backgroundFar);
+    
+    // character
+    var characterTexture = PIXI.Texture.fromImage("img/test.png");
+    var character = new PIXI.Sprite(characterTexture);
+    character.position.x = 100;
+    character.position.y = 100;
+    character.anchor.x = 0.5;
+    character.anchor.y = 0.5;
+    stage.addChild(character);
+    
+    // animate
+    requestAnimFrame(animate);
+    function animate() {
+	// update positions
+	character.rotation += 0.01;
+	backgroundFar.tilePosition.x -= 0.5;
+	
+	// render
+	renderer.render(stage);
+	requestAnimFrame(animate);
+    }
 });
