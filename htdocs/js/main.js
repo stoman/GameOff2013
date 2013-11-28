b31150fdc599af5dee6448bbc31a5149d5208142
Stefan Toman <stefan.toman@gmail.com>
2013-11-29 00:50:18 +0100

    implement new graphics

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -8,15 +8,28 @@ $(window).load(function() {
 	    );
     var stage = new PIXI.Stage(0x66FF99);
     
-    // far background
-    var backgroundFarTexture = PIXI.Texture.fromImage("img/github_game_off_2013_resized.png");
-    var backgroundFar = new PIXI.TilingSprite(
-	    backgroundFarTexture,
+    // far background 2
+    var backgroundFar2Texture = PIXI.Texture.fromImage("img/github_game_off_2013_resized.png");
+    var backgroundFar2 = new PIXI.TilingSprite(
+	    backgroundFar2Texture,
 	    2048,
 	    512
     );
+    backgroundFar2.position.x = 0;
+    backgroundFar2.position.y = -150;
+    backgroundFar2.tilePosition.x = 0;
+    backgroundFar2.tilePosition.y = 0;
+    stage.addChild(backgroundFar2);
+
+    // far background 
+    var backgroundFarTexture = PIXI.Texture.fromImage("img/background_far.png");
+    var backgroundFar = new PIXI.TilingSprite(
+	    backgroundFarTexture,
+	    4*1024,
+	    4*128
+    );
     backgroundFar.position.x = 0;
-    backgroundFar.position.y = 0;
+    backgroundFar.position.y = canvas.height() - 4*128;
     backgroundFar.tilePosition.x = 0;
     backgroundFar.tilePosition.y = 0;
     stage.addChild(backgroundFar);
@@ -33,23 +46,38 @@ $(window).load(function() {
     backgroundMid.tilePosition.x = 0;
     backgroundMid.tilePosition.y = 0;
     stage.addChild(backgroundMid);
-    
+
+    // front background
+    var backgroundFrontTexture = PIXI.Texture.fromImage("img/background_front.png");
+    var backgroundFront = new PIXI.TilingSprite(
+	    backgroundFrontTexture,
+	    4*1024,
+	    4*128
+    );
+    backgroundFront.position.x = 0;
+    backgroundFront.position.y = canvas.height() - 4*128;
+    backgroundFront.tilePosition.x = 0;
+    backgroundFront.tilePosition.y = 0;
+    stage.addChild(backgroundFront);
+
     // character
     var characterTexture = PIXI.Texture.fromImage("img/test.png");
     var character = new PIXI.Sprite(characterTexture);
     character.position.x = 100;
-    character.position.y = 100;
+    character.position.y = 475;
     character.anchor.x = 0.5;
-    character.anchor.y = 0.5;
+    character.anchor.y = 1;
     stage.addChild(character);
     
     // animate
     requestAnimFrame(animate);
     function animate() {
 	// update positions
-	character.rotation += 0.01;
-	backgroundFar.tilePosition.x -= 0.5;
-	backgroundMid.tilePosition.x -= 2;
+	//character.rotation += 0.01;
+	backgroundFar2.tilePosition.x -= 0.9;
+	backgroundFar.tilePosition.x -= 1.2;
+	backgroundMid.tilePosition.x -= 1.8;
+	backgroundFront.tilePosition.x -= 2.7;
 	
 	// render
 	renderer.render(stage);
